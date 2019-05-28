3f7da8c735c0fee676236d19cc42cacb8858daf2
unknown <kodipallikumar@gmail.com>
2019-05-28 22:41:51 +0530

    first commit

---
+++ com/example/demo/DemoJenkin.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class DemoJenkin {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Welcome to jenkins");
+	}
+
+}
---
+++ com/example/demo/SampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+//@SpringBootApplication
+public class SampleApplication {
+	
+	
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SampleApplication.class, args);
+		System.out.println("Hello World");
+		
+	}
+
+}
